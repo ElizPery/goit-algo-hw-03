438853a777c0db96b093bced182117d2245810db
Elizabeth Pereriadkina <el.pereryadkina@gmail.com>
2025-01-20 21:00:05 +0000

    add koch snowflake

---
+++ koch_snowflake.py
@@ -0,0 +1,35 @@
+import turtle
+
+def koch_curve(t, order, size):
+    if order == 0:
+        t.forward(size)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_curve(t, order - 1, size / 3)
+            t.left(angle)
+
+def draw_koch_curve(order, size=300):
+    window = turtle.Screen()
+    window.bgcolor("white")
+
+    t = turtle.Turtle()
+    t.speed(0)  
+    t.penup()
+    t.goto(-size / 2, 0)
+    t.pendown()
+
+    koch_curve(t, order, size)
+
+    window.mainloop()
+
+order = input("Please enter number of recursions >>> ")
+try:
+    order = int(order.strip())
+    if(order <= 0):
+        raise ValueError
+except:
+    print('Something went wrong. Please enter number more than 0!')
+    order = 3
+    print('By default drawn for 3!')
+
+draw_koch_curve(order)
